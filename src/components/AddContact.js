a88b91099c060bdc5f85fd8dc92ca866d1de7644
Amadeus <amadeusarkhammorningstar@gmail.com>
2020-09-30 23:42:13 +0600

    commented all lines in AddContacts.js

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-export default function AddContact(){   
-    const [data, setPerson] = useState([
-        {
-            name: '',
-            age: '',
-            city: ''
-        }
-    ])
-    console.log(data.name)
-    return(
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            setPerson([{name: 1}])
-                setPerson(() => {
-                    let res = data
-                    res.name = e.target[0].value
-                    res.age = e.target[1].value
-                    res.city = e.target[2].value
-                    return res
-                })
-            // setPerson([...data, {name: e.target[0], age: e.target[1], city: e.target[2]}])
-        }}>
+// export default function AddContact(){   
+//     const [data, setPerson] = useState([
+//         {
+//             name: '',
+//             age: '',
+//             city: ''
+//         }
+//     ]);
+//     console.log(data.name);
+//     return(
+//         <form onSubmit={(e) => {
+//             e.preventDefault()
+//             setPerson([{name: 1}])
+//                 setPerson(() => {
+//                     let res = data
+//                     res.name = e.target[0].value
+//                     res.age = e.target[1].value
+//                     res.city = e.target[2].value
+//                     return res
+//                 })
+//             // setPerson([...data, {name: e.target[0], age: e.target[1], city: e.target[2]}])
+//         }}>
                         
-                <input type="text" name ="name "placeholder="Name" key="name"/>
-                <input type="text" name = "age" placeholder="Age" key="age"/>
-                <input type="text" name = "city" placeholder="City" key="city"/>
-                <button>add</button>
-                {/* {data} */}
-        </form>
-    );
-}
+//                 <input type="text" name ="name "placeholder="Name" key="name"/>
+//                 <input type="text" name = "age" placeholder="Age" key="age"/>
+//                 <input type="text" name = "city" placeholder="City" key="city"/>
+//                 <button>add</button>
+                
+//         </form>
+//     );
+// }
